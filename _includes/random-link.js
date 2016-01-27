830d5f1dee0247d48aa9f39035666b26a9d5526d
Morgan Jeffrey <morgan@fsw.com>
2016-01-27 09:04:51 -0700

    changed array in random link

--- _includes/random-link.js
+++ _includes/random-link.js
@@ -1,14 +1,14 @@
-var randomlinks=new Array()
-
-randomlinks[0]="http://www.station26brewing.co/"
-randomlinks[1]="http://www.comradebrewing.com/"
-randomlinks[2]="http://www.barnettandsonbrewing.com/"
-randomlinks[3]="http://www.fremontbrewing.com/"
-randomlinks[4]="http://www.horseanddragonbrewing.com/"
-randomlinks[5]="http://fatebrewingcompany.com/"
+var randomlinks=[
+  "http://www.station26brewing.co/",
+  "http://www.comradebrewing.com/",
+  "http://www.barnettandsonbrewing.com/",
+  "http://www.fremontbrewing.com/",
+  "http://www.horseanddragonbrewing.com/",
+  "http://fatebrewingcompany.com/"
+]
 
 function randomlink(){
-window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
+  window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
 }
 
 /* 
